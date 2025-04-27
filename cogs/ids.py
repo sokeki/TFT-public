bf92dc0db4f9651fd796625037e6027ce8265d27
sokeki <83179557+sokeki@users.noreply.github.com>
2025-04-27 21:30:01 +0100

    added id check

---
+++ cogs/ids.py
@@ -0,0 +1,60 @@
+import discord
+import pandas as pd
+from discord.ext import tasks
+import os
+from dotenv import load_dotenv
+from riotwatcher import TftWatcher, RiotWatcher
+import urllib
+from discord.ext import commands
+from pymongo_get_database import get_database
+
+dbname = get_database()
+
+user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
+headers = {
+    "User-Agent": user_agent,
+}
+url = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/companions.json"
+request = urllib.request.Request(url, None, headers)
+companions = urllib.request.urlopen(request)
+
+load_dotenv()
+bot = discord.Bot()
+api_key = str(os.getenv("RIOT"))
+print(api_key)
+tft_watcher = TftWatcher(api_key)
+riot_watcher = RiotWatcher(api_key)
+pd.set_option("display.float_format", "{:.0f}".format)
+
+
+class Ids(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @tasks.loop(hours=12)
+    async def ids(self):
+        print("checking ranks...")
+        collection_name = dbname["users"]
+        data_raw = collection_name.find()
+        data = pd.DataFrame(data_raw)
+        for i in range(len(data)):
+            riot_id = data.iloc[i]["_id"]
+            region = data.iloc[i]["region"]
+            name = data.iloc[i]["name"]
+            tag = data.iloc[i]["tag"]
+            account = riot_watcher.account.by_riot_id(region, name, tag)
+            new_id = account["puuid"]
+            if riot_id != id:
+                query = {"$and": [{"region": region, "name": name, "tag": tag}]}
+                new_values = {"$set": {"_id": new_id}}
+                collection_name.update(query, new_values)
+                print("Updated ID")
+            new_name = account["gameName"]
+            new_tag = account["tagLine"]
+            if new_name != name or new_tag != tag:
+                query = {"_id": riot_id}
+                new_values = {"$set": {"name": new_name}}
+                collection_name.update(query, new_values)
+                new_values = {"$set": {"tag": new_tag}}
+                collection_name.update(query, new_values)
+                print("Updated name and tag")
--- main.py
+++ main.py
@@ -5,25 +5,29 @@ from dotenv import load_dotenv
 load_dotenv()
 bot = discord.Bot()
 
-cogs_list = [
-    'add',
-    'list',
-    'lookup',
-    'remove',
-    'stats'
-]
+cogs_list = ["add", "list", "lookup", "remove", "stats"]
 
 for cog in cogs_list:
-    bot.load_extension(f'cogs.{cog}')
+    bot.load_extension(f"cogs.{cog}")
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready!")
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="your LP"))
+    await bot.change_presence(
+        status=discord.Status.online,
+        activity=discord.Activity(type=discord.ActivityType.watching, name="your LP"),
+    )
 
     lookup_cog = bot.get_cog("Lookup")
     if lookup_cog and not lookup_cog.lookup.is_running():
         lookup_cog.lookup.start()
         print("Lookup loop started")
 
-bot.run(os.getenv('TOKEN'))
+    id_cog = bot.get_cog("Ids")
+    if id_cog and not id_cog.ids.is_running():
+        id_cog.ids.start()
+        print("Id check loop started")
+
+
+bot.run(os.getenv("TOKEN"))
